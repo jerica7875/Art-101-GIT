c926e937b2ffb0e28fbf54b30c11b98c8d5df85d
jerica7875 <66146329+jerica7875@users.noreply.github.com>
2025-11-17 23:14:30 -0800

    hhj

---
+++ Environ/lab08.js
@@ -0,0 +1,42 @@
+let number = 0;
+let images = [
+  "https://images.unsplash.com/photo-1475727946784-2890c8fdb9c8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1484",
+  "https://images.unsplash.com/photo-1498015583783-4abcab4a760a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
+];
+
+
+// When the button with id="no" is clicked
+$("#no").click(function() {
+  // Change the image source
+  number = (number + 1) % images.length;
+  $("#hell").attr("src", images[number])
+ $("#eagle").animate({left: '1000px'})
+
+  
+})
+
+$("#yes").click(function(){
+ 
+  $("#eagle2").animate({left: '400px'});
+
+})
+
+$(".test-div").hover(function(){
+  $("#campaign").hide()
+})
+
+$("#hell").mousemove(function(event) {
+  $("#hell").css({
+    left: event.pageX - 60,
+    top: event.pageY - 60,
+    position: "absolute"
+ }) });
+ 
+$("#hell").dblclick(function() {
+  alert("now you die!");
+});
+
+$("#heaven").dblclick(function() {
+  alert("now you live!");
+
+});
